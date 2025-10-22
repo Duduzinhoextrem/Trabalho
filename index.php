a7cb553410f4d1cb8a4549fa43db0f743161e23d
Eduardo Lima <109998542+Duduzinhoextrem@users.noreply.github.com>
2025-10-22 19:23:48 -0300

    alteração titulo

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8">
 </head>
 <body>
-	<h2>Cadastro de Clientes</h2>
+	<h2>Cadastro de dudu</h2>
 	<a href="adicionar.php"><button>Adicionar</button></a>
 	<a href="pesquisar.php"><button>Pesquisar</button></a>
 	<br />
